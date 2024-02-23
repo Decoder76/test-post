2d190ae17d2e62ad017d055a395ddd3d760bfcbd
Lokesh <60029008+Decoder76@users.noreply.github.com>
2024-02-23 12:17:32 +0000

    Remove_tweet_model

--- db/migrate/20240222124316_create_tweets.rb
+++
@@ -1,9 +0,0 @@
-class CreateTweets < ActiveRecord::Migration[7.1]
-  def change
-    create_table :tweets do |t|
-      t.text :content
-
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,5 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_22_124316) do
-  create_table "tweets", force: :cascade do |t|
-    t.text "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
+ActiveRecord::Schema[7.1].define(version: 0) do
 end
